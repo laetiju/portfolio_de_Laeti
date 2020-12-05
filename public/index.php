8179ea4bacc855db34e74080bcdda99d8e4ab3ed
John Doe <latitiajulien91@gmail.com>
2020-12-05 23:18:03 +0100

    startappli

--- public/index.php
+++ public/index.php
@@ -3,9 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Les Dessin de Laeti</title>
 </head>
 <body>
+<div class="container">
+<div class="hero"></div>
+<div class="hero"></div>
+<div class="section"></div>
+<div class="footer"></div>
+
+</div>
     
 </body>
 </html>
